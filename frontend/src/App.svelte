cebfd832609cc3bc3aed6fdd6d6e43939b1f795b
Rittvic Ray <rittvicr@gmail.com>
2023-03-04 17:18:19 -0600

    Added WIP dropdown component

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,12 +1,11 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+<script>
+  import DropdownLanguage from "./lib/DropdownLanguage.svelte";
 </script>
 
 <main>
   <div class="flex flex-row w-screen h-screen">
     <div class="flex-grow border-r-2 border-white">
-      left side
+        <DropdownLanguage></DropdownLanguage>
     </div>
     <div class="w-2/6 flex-none">
       right side
---
+++ frontend/src/lib/DropdownLanguage.svelte
@@ -0,0 +1,13 @@
+<script lang="js">
+  import {Button, Dropdown, DropdownItem} from 'flowbite-svelte'
+  let selectedLanguage = "Select a language";
+  let langs = ["Python", "Java", "Javascript", "Rust", "C", "C++", "C#"];
+</script>
+
+<Button color="blue">{selectedLanguage}</Button>
+<Dropdown id="dropdown">
+    {#each langs as lang}
+      <DropdownItem on:click={() => selectedLanguage = lang}>{lang}</DropdownItem>
+    {/each}
+</Dropdown>
+
