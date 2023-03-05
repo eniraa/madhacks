b4eed1e5d5787d9933d543fc29010ca6a3d82a95
Raymond Zhao <stinggyray@gmail.com>
2023-03-04 18:47:52 -0600

    update dropdown

--- frontend/src/lib/LeftComponent.svelte
+++ frontend/src/lib/LeftComponent.svelte
@@ -1,17 +1,29 @@
 <script lang="js">
-    import { Button, Dropdown, DropdownItem } from "flowbite-svelte";
+    import { Button, Dropdown, DropdownItem, Chevron } from "flowbite-svelte";
     import CodeInput from "./CodeInput.svelte";
 
     export let language = "Python";
     let langs = ["Python", "Java", "JavaScript", "Rust", "C", "C++", "C#"];
     let text = "";
+
+    let dropdownOpen = false;
 </script>
 
-<Button color="black">{language}</Button>
-<Dropdown>
-    {#each langs as lang}
-        <DropdownItem class="w-1/2" on:click={() => language = lang}>{lang}</DropdownItem>
-    {/each}
-</Dropdown>
+<div class="flex items-center mx-4 h-16">
+    <div class="flex-grow">
+        <h1 class="text-xl">Code Analyzer</h1>
+    </div>
+    <div class="flex-none relative">
+        <Button color="blue" size="xs"><Chevron>{language}</Chevron></Button>
+        <Dropdown class="py-1 w-24" bind:open={dropdownOpen}>
+            {#each langs as lang}
+                <DropdownItem on:click={() => {
+                    language = lang;
+                    dropdownOpen = false;
+                }}>{lang}</DropdownItem>
+            {/each}
+        </Dropdown>
+    </div>
+</div>
 <CodeInput language={language} />
 
