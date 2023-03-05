1aace26d6abc8eb1db6e549e2a033250a29e21cb
Raymond Zhao <stinggyray@gmail.com>
2023-03-04 19:13:57 -0600

    add small text

--- frontend/src/lib/InputInput.svelte
+++ frontend/src/lib/InputInput.svelte
@@ -5,7 +5,7 @@
 
 <div class="m-4 h-1/3 flex">
   <div class="flex-1 flex flex-col mr-4">
-    <h2 class="text-xl mb-2">Input</h2>
+    <h2 class="text-xl mb-2">Input&nbsp;&nbsp;<span class="text-xs text-neutral-400"> (this gets fed to stdin)</span></h2>
     <textarea spellcheck="false"
               class="flex-grow p-4 code-input bg-neutral-800 rounded-md focus:ring-0 focus:outline-none border-2 border-neutral-500 focus:border-neutral-500"
               placeholder="Your input here..."></textarea>
@@ -15,7 +15,7 @@
     </div>
   </div>
   <div class="flex-1 ml-4 flex flex-col">
-    <h2 class="text-xl mb-2">Output</h2>
+    <h2 class="text-xl mb-2">Output&nbsp;&nbsp;<span class="text-xs text-neutral-400"> (stdout)</span></h2>
     <div class="flex-grow p-4 code-input bg-neutral-800 rounded-md border-2 border-neutral-700">{output}</div>
   </div>
 </div>
