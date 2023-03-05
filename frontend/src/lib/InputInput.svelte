0035846c7e5066ab5c17db62a59386da377e8c94
Rittvic Ray <rittvicr@gmail.com>
2023-03-04 21:21:22 -0600

    add spinner

--- frontend/src/lib/InputInput.svelte
+++ frontend/src/lib/InputInput.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
-  import { Button } from "flowbite-svelte";
+  import { Button, Spinner } from "flowbite-svelte";
   export let output = "the wrong answer";
+  export let label = "Ready to test?"
+  export let buttonLabel = "Run";
+
+  export let showSpinner = false;
+  function runCode() {
+    label = "Running...";
+    showSpinner = true;
+  }
 </script>
 
 <div class="m-4 h-1/3 flex">
@@ -10,8 +18,11 @@
               class="flex-grow p-4 code-input bg-neutral-800 rounded-md focus:ring-0 focus:outline-none border-2 border-neutral-500 focus:border-neutral-500"
               placeholder="Your input here..."></textarea>
     <div class="flex flex-row mt-4 items-center">
-      <div class="flex-grow flex flex-row-reverse mr-4 items-center"><p class="text-sm">Ready to test?</p></div>
-      <Button color="blue" size="xs">Run</Button>
+      <div class="flex-grow flex flex-row-reverse mr-4 items-center"><p class="text-sm">{label}</p></div>
+      {#if showSpinner}
+       <Spinner class="mr-4" id="spinner" size={4} />
+      {/if}
+      <Button color="blue" disabled={showSpinner} size="xs" on:click={() => runCode()}>{buttonLabel}</Button>
     </div>
   </div>
   <div class="flex-1 ml-4 flex flex-col">
