174a6ad38bddac4c3d905025a78dbde01dbd7596
eniraa <accounts+github@eji.moe>
2023-03-05 10:12:28 -0600

    inject memory code in a new file instead of execing

--- backend/suite/memory.py
+++ backend/suite/memory.py
@@ -1,19 +1,28 @@
+import subprocess
+
 def memory_profile():
     """Return a dict with memory usage information."""
     
     with open("/app/main.py") as f:
         code = f.read()
-
-    import tracemalloc
-
-    tracemalloc.start()
-
-    exec(code)
-
-    with open("/app/feedback/memory.txt", "w") as f:
-        peak = tracemalloc.get_traced_memory()[1]
-        f.write(f"{peak}")
     
-    tracemalloc.stop()
+    new_code = f"""
+import tracemalloc
+
+tracemalloc.start()
+
+{code}
+
+with open("/app/feedback/memory.txt", "w") as f:
+    peak = tracemalloc.get_traced_memory()[1]
+    f.write(str(peak))
+
+tracemalloc.stop()
+"""
+
+    with open("/app/main_mem.py", "w") as f:
+        f.write(new_code)
+    
+    subprocess.run(f"cat /app/in | python3 /app/main_mem.py", shell=True)
 
 memory_profile()
